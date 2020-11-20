e453f477955ec758953b98005c0f1885bbb74e88
Hannarong Klinjan <hannarong98@hotmail.com>
2020-11-20 04:44:51 +0100

    Update bucket again

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "static-site" {
-  project = "pgr301-exam-hannarong-296202"
+  project = "pgr301-exam-hannarong-2962023"
   name = "pgr301-exam-bucket-2991"
   location = "US"
 }
