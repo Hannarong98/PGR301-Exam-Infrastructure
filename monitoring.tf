3997f850673cd9f5f57b9afabcc44a59edbe18b3
Hannarong Klinjan <hannarong98@hotmail.com>
2020-11-21 01:36:38 +0100

    Add statuscake required provider

--- monitoring.tf
+++ monitoring.tf
@@ -3,10 +3,20 @@ provider "statuscake" {
   apikey = var.status_cake_api_key
 }
 
+
+
 resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "DimFlow"
   website_url = google_cloud_run_service.pgr301-exam.status[0].url
   check_rate = 300
   contact_group = ["195453"]
+}
+
+terraform {
+  required_providers {
+    statuscake = {
+      source = "terraform-providers/statuscake"
+    }
+  }
 }
